a72341a19a0a4786b6f1f380628469aefacd3967
Vitali Varabuey <72112909+Vorobey1992@users.noreply.github.com>
2023-04-03 17:20:51 +0300

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,34 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Product))
+            {
+                return false;
+            }
+
+            var other = (Product)obj;
+
+            return this.Name == other.Name && this.Price == other.Price;
+        }
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 23 + this.Name.GetHashCode();
+            hash = hash * 23 + this.Price.GetHashCode();
+            return hash;
+        }
+    }
+}
